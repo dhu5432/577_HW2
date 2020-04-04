2515a1a92d0bc8b59dc87fef25a633533c23ba8b
Daniel Hu <dhu5432@gmail.com>
2020-04-03 21:24:23 -0400

    Ready for submission

--- main.py
+++ main.py
@@ -20,21 +20,28 @@ from gensim.test.utils import datapath
 You may also use other parsing functions, but ONLY for parsing and ONLY from that file.
 '''
 embed_dimension = 300
-epochs = 1
-
+epochs = 30
+LSTM_flag = False
 
 class NeuralNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, LSTM):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(2*embed_dimension+4, 400)
+        LSTM_flag = LSTM
+        if LSTM:
+            self.lstm = nn.LSTM(embed_dimension+4, 2*embed_dimension+4, bidirectional=True)
+            self.fc1 = nn.Linear(embed_dimension+4, 400)
+        else:
+            self.fc1 = nn.Linear(2*embed_dimension+4, 400)
         self.act1 = nn.ReLU()
         self.fc2 = nn.Linear(400, 200)
         self.act2 = nn.Sigmoid()
         self.fc3 = nn.Linear(200, 4)
         self.act3 = nn.LogSoftmax()
 
-    def forward(self, input_):
+    def forward(self, input_, sentence=None):
+        if LSTM_flag:
+            input_, _ = self.lstm(sentence)
         a1 = self.fc1(input_)
         h1 = self.act1(a1)
         a2 = self.fc2(h1)
@@ -104,6 +111,7 @@ def argmax(vec):
     #return idx.item()
 
 def main():
+    torch.device('cpu')
     false_positive = 0
     false_negative = 0
     true_positive = 0
@@ -171,9 +179,9 @@ def main():
 
         start = timer()
         # Training
-        net = NeuralNet()
-        opt = optim.Adam(net.parameters(), lr=0.01, betas=(0.9, 0.999))
-        criterion = nn.NLLLoss()
+        net = NeuralNet(False)
+        opt = optim.Adam(net.parameters(), lr=0.01, betas=(0.9, 0.999), weight_decay=0.1)
+        criterion = nn.NLLLoss(torch.Tensor([1.2, 1.2, 1.2, 0.7]))
 
         for epoch in range(epochs):
             for sentence_num in range(len(train_set)):
@@ -205,7 +213,7 @@ def main():
 
         # Viterbi: Testing
         for sentence_num in range(len(test_set)):
-            init_vvars = torch.full((1, 6), -10000)
+            init_vvars = torch.full((1, 5), -10000)
             init_vvars[0][4] = 0
             backpointers = []
             forward_var = init_vvars
@@ -220,11 +228,11 @@ def main():
                 bptrs_t = []
                 viterbivars_t = []
 
-                for next_tag in range(4):
+                for tagg in range(4):
                     if feat == 0:
                         arbitrary_embed = dictionary_of_word_embeddings[dictionary_of_words_reversed[' ']]
                         concat = torch.cat((arbitrary_embed, word_embed,
-                                            dictionary_of_label_embeddings[dictionary_of_labels_index[next_tag]]), 1)
+                                            dictionary_of_label_embeddings[dictionary_of_labels_index[tagg]]), 1)
                         pred = net(autograd.Variable(concat))
 
                     else:
@@ -234,7 +242,7 @@ def main():
                         except KeyError:
                             prev_word_embed = dictionary_of_word_embeddings[dictionary_of_words_reversed[' ']]
                         prev_label_embed = dictionary_of_label_embeddings[dictionary_of_labels_index[
-                            next_tag]]  # dictionary_of_label_embeddings[validation_set[sentence_num]['ts_raw_tags'][word_num - 1]]
+                            tagg]]  # dictionary_of_label_embeddings[validation_set[sentence_num]['ts_raw_tags'][word_num - 1]]
 
                         concat = torch.cat((prev_word_embed, word_embed, prev_label_embed), 1)
                         pred = net(autograd.Variable(concat))
@@ -332,7 +340,7 @@ def main():
             for tag in sentence['ts_raw_tags']:
                 tags.add(tag)
 
-        wv_from_bin = KeyedVectors.load_word2vec_format("w2v.bin", binary=True)
+        wv_from_bin = KeyedVectors.load_word2vec_format("/homes/cs577/hw2/w2v.bin", binary=True)
 
         # Create Label Embeddings
         dictionary_of_labels = {}
@@ -355,9 +363,9 @@ def main():
 
         start = timer()
         # Training
-        net = NeuralNet()
+        net = NeuralNet(False)
         opt = optim.Adam(net.parameters(), lr=0.01, betas=(0.9, 0.999))
-        criterion = nn.NLLLoss()
+        criterion = nn.CrossEntropyLoss()
 
         for epoch in range(epochs):
             print("Epoch {}".format(epoch+1))
@@ -412,11 +420,11 @@ def main():
                 bptrs_t = []
                 viterbivars_t = []
 
-                for next_tag in range(4):
+                for tagg in range(4):
                     if feat == 0:
                         arbitrary_embed = torch.zeros([1,300])
                         concat = torch.cat((arbitrary_embed, word_embed,
-                                            dictionary_of_label_embeddings[dictionary_of_labels_index[next_tag]]), 1)
+                                            dictionary_of_label_embeddings[dictionary_of_labels_index[tagg]]), 1)
                         pred = net(autograd.Variable(concat))
 
                     else:
@@ -426,15 +434,13 @@ def main():
                         except KeyError:
                             prev_word_embed = torch.zeros([1, 300])
                         prev_label_embed = dictionary_of_label_embeddings[dictionary_of_labels_index[
-                            next_tag]]  # dictionary_of_label_embeddings[validation_set[sentence_num]['ts_raw_tags'][word_num - 1]]
+                            tagg]]  # dictionary_of_label_embeddings[validation_set[sentence_num]['ts_raw_tags'][word_num - 1]]
 
                         concat = torch.cat((prev_word_embed, word_embed, prev_label_embed), 1)
                         pred = net(autograd.Variable(concat))
 
                     next_tag_var = forward_var + torch.cat((pred, torch.zeros([1, 2])), 1)
-                    #print(next_tag_var)
                     best_tag_id = argmax(next_tag_var)
-                    #print(best_tag_id)
                     bptrs_t.append(best_tag_id)
                     viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
 
@@ -510,12 +516,193 @@ def main():
         print("Recall: {}".format(recall))
         print("F1: {}".format(f1))
 
+    if vars(args)['option'] == 3:
+        train_set = read_data(path=args.train_file)
+        test_set = read_data(path=args.test_file)
 
+        #train_set1 = train_set[0:1692]
+        #validation_set = train_set[1692:len(train_set)]
+        #train_set = train_set1
 
+        words = set()
+        for sentence in train_set:
+            split = sentence['sentence'].split()
+            for word in split:
+                words.add(word)
+        tags = set()
+        for sentence in train_set:
+            for tag in sentence['ts_raw_tags']:
+                tags.add(tag)
 
+        wv_from_bin = KeyedVectors.load_word2vec_format("/homes/cs577/hw2/w2v.bin", binary=True)
 
+        # Create Label Embeddings
+        dictionary_of_labels = {}
+        dictionary_of_labels_index = {}
 
+        dictionary_of_labels_index[0] = 'T-NEU'
+        dictionary_of_labels_index[1] = 'T-POS'
+        dictionary_of_labels_index[2] = 'T-NEG'
+        dictionary_of_labels_index[3] = 'O'
 
+        dictionary_of_label_embeddings ={}
+        dictionary_of_labels['T-NEU'] = [1, 0, 0, 0]  # T-NEU
+        dictionary_of_labels['T-POS'] = [0, 1, 0, 0]  # T-POS
+        dictionary_of_labels['T-NEG'] = [0, 0, 1, 0]  # T-NEG
+        dictionary_of_labels['O'] = [0, 0, 0, 1]  # O
+        dictionary_of_labels['START'] = [0, 0, 0, 0]  # START
+
+        for key in dictionary_of_labels.keys():
+            dictionary_of_label_embeddings[key] = torch.FloatTensor([dictionary_of_labels[key]])
+
+        start = timer()
+        # Training
+        net = NeuralNet(True)
+        opt = optim.Adam(net.parameters(), lr=0.01, betas=(0.9, 0.999))
+        criterion = nn.CrossEntropyLoss()
+
+        for epoch in range(epochs):
+            print("Epoch {}".format(epoch+1))
+            for sentence_num in range(len(train_set)):
+                for word_num in range(len(train_set[sentence_num]['sentence'].split())):
+                    word = train_set[sentence_num]['sentence'].split()[word_num]
+                    try:
+                        word_embed = torch.from_numpy(wv_from_bin[word])
+                    except KeyError:
+                        word_embed = torch.zeros([1, 300])
+
+                    gold_label_embed = dictionary_of_label_embeddings[train_set[sentence_num]['ts_raw_tags'][word_num]]
+                    if word_num == 0:
+                        arbitrary_embed = torch.zeros([1, 300])
+                        concat = torch.cat((word_embed.view(1, 300), dictionary_of_label_embeddings['START']), 1)
+
+                    else:
+                        prev_label_embed = dictionary_of_label_embeddings[train_set[sentence_num]['ts_raw_tags'][word_num-1]]
+                        concat = torch.cat((word_embed.view(1, 300), prev_label_embed), 1)
+                    opt.zero_grad()
+                    word_embed = autograd.Variable(concat)
+                    pred = net(word_embed)
+                    gold_label_class = torch.max(gold_label_embed, 1)[1]
+
+                    loss = criterion(pred, gold_label_class)
+                    loss.backward()
+                    opt.step()
+
+        end = timer()
+        print(end - start)
+
+        # Viterbi: Testing
+        for sentence_num in range(len(test_set)):
+            init_vvars = torch.full((1, 6), -10000)
+            init_vvars[0][4] = 0
+            backpointers = []
+            forward_var = init_vvars
+            for feat in range(len(test_set[sentence_num]['sentence'].split())):
+
+                word = test_set[sentence_num]['sentence'].split()[feat]
+                try:
+                    word_embed = torch.from_numpy(wv_from_bin[word]).view(1, 300)
+                except KeyError:
+                    word_embed = torch.zeros([1, 300])
+
+                bptrs_t = []
+                viterbivars_t = []
+
+                for tagg in range(4):
+                    if feat == 0:
+                        arbitrary_embed = torch.zeros([1,300])
+                        concat = torch.cat((word_embed,
+                                            dictionary_of_label_embeddings[dictionary_of_labels_index[tagg]]), 1)
+                        pred = net(autograd.Variable(concat))
+
+                    else:
+                        prev_word = test_set[sentence_num]['sentence'].split()[feat - 1]
+                        try:
+                            prev_word_embed = torch.from_numpy(wv_from_bin[prev_word]).view(1, 300)
+                        except KeyError:
+                            prev_word_embed = torch.zeros([1, 300])
+                        prev_label_embed = dictionary_of_label_embeddings[dictionary_of_labels_index[
+                            tagg]]  # dictionary_of_label_embeddings[validation_set[sentence_num]['ts_raw_tags'][word_num - 1]]
+
+                        concat = torch.cat((word_embed, prev_label_embed), 1)
+                        pred = net(autograd.Variable(concat))
+
+                    next_tag_var = forward_var + torch.cat((pred, torch.zeros([1, 2])), 1)
+                    best_tag_id = argmax(next_tag_var)
+                    bptrs_t.append(best_tag_id)
+                    viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
+
+                forward_var = torch.cat((viterbivars_t)).view(1, -1)
+                forward_var = torch.cat((forward_var, torch.zeros([1, 2])), 1)
+                backpointers.append(bptrs_t)
+            terminal_var = forward_var
+            best_tag_id = argmax(terminal_var)
+            path_score = terminal_var[0][best_tag_id]
+
+            best_path = [best_tag_id]
+            #print(best_tag_id)
+            for bptrs_t in reversed(backpointers):
+                best_tag_id = bptrs_t[best_tag_id]
+                best_path.append(best_tag_id)
+            start = best_path.pop()
+            best_path.reverse()
+            #print(best_path)
+
+            for word_num in range(len(test_set[sentence_num]['ts_raw_tags'])):
+                predicted_tag = dictionary_of_labels_index[best_path[word_num]]
+                true_tag = test_set[sentence_num]['ts_raw_tags'][word_num]
+                if true_tag == 'T-NEU' and predicted_tag == 'T-NEU':
+                    true_positive += 1
+                elif true_tag == 'T-NEU' and predicted_tag == 'T-POS':
+                    false_negative += 1
+                    false_positive += 1
+                elif true_tag == 'T-NEU' and predicted_tag == 'T-NEG':
+                    false_negative += 1
+                    false_positive += 1
+                elif true_tag == 'T-NEU' and predicted_tag == 'O':
+                    false_negative += 1
+
+                elif true_tag == 'T-POS' and predicted_tag == 'T-NEU':
+                    false_negative += 1
+                    false_positive += 1
+                elif true_tag == 'T-POS' and predicted_tag == 'T-POS':
+                    true_positive += 1
+                elif true_tag == 'T-POS' and predicted_tag == 'T-NEG':
+                    false_negative += 1
+                    false_positive += 1
+                elif true_tag == 'T-POS' and predicted_tag == 'O':
+                    false_negative += 1
+
+                elif true_tag == 'T-NEG' and predicted_tag == 'T-NEU':
+                    false_negative += 1
+                    false_positive += 1
+                elif true_tag == 'T-NEG' and predicted_tag == 'T-POS':
+                    false_negative += 1
+                    false_positive += 1
+                elif true_tag == 'T-NEG' and predicted_tag == 'T-NEG':
+                    true_positive += 1
+                elif true_tag == 'T-NEG' and predicted_tag == 'O':
+                    false_negative += 1
+
+                elif true_tag == 'O' and predicted_tag == 'T-NEU':
+                    false_positive += 1
+                elif true_tag == 'O' and predicted_tag == 'T-POS':
+                    false_positive += 1
+                elif true_tag == 'O' and predicted_tag == 'T-NEG':
+                    false_positive += 1
+                elif true_tag == 'O' and predicted_tag == 'O':
+                    true_negative += 1
+                else:
+                    print(true_tag)
+                    print(predicted_tag)
+                    print("SHOULDN'T BE HERE")
+        print(true_positive)
+        precision = true_positive / (true_positive + false_positive)
+        recall = true_positive / (true_positive + false_negative)
+        f1 = (2 * precision * recall) / (precision + recall)
+        print("Precision: {}".format(precision))
+        print("Recall: {}".format(recall))
+        print("F1: {}".format(f1))
 
 
 
